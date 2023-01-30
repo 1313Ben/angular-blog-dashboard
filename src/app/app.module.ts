59ca9dce7beb3f4d3e6f68a20ae16f07a47f2a94
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-01-30 20:25:42 +0100

    Toastr messag succesfully implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +28,9 @@ import { CategoriesComponent } from './categories/categories.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    FormsModule
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor(private fireStore: AngularFirestore) { }
+  constructor(private fireStore: AngularFirestore, private toastr: ToastrService) { }
 
   saveData(categoryData: any) {
     this.fireStore.collection('categories').add(categoryData)
       .then(docRef => {
-        console.log(docRef)
+        console.log(docRef);
+        this.toastr.success('Data Insert Successfully');
       })
     .catch(err => {console.log(err)})
     console.log(categoryData)
