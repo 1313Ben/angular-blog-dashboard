6e6cf89d6d649cefe7bdc69e9222075b2b02dd24
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-02-07 20:36:44 +0100

    Feature: Post image preview is done.

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -7,12 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
-  permaLink: string = ""
+  permaLink: string = "";
+  imgSrc: any = "./assets/placeholder-image.png";
+  selectedImg: any;
 
   onTitleChanged($event:any) {
     //console.log($event.target.value)
     const title = $event.target.value;
     this.permaLink = title.replace(/\s/g, "-");
     console.log(this.permaLink)
+  }
+
+  showPreview($event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result
+    }
+
+    reader.readAsDataURL($event.target.files[0])
+    this.selectedImg = $event.target.files[0]
   }
 }
