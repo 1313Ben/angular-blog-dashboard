3f0f484ef4faf42a8e0ff940852e5117bb41dd49
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-02-08 09:24:58 +0100

    Selectable categories on new post implemented

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { CategoriesService } from './../../services/categories.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit{
 
   permaLink: string = "";
   imgSrc: any = "./assets/placeholder-image.png";
   selectedImg: any;
+
+  categories: Array<object> | any;
+
+  constructor(private categoryService: CategoriesService) { }
+  
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe(val => {
+      this.categories = val;
+    })
+  }
 
   onTitleChanged($event:any) {
     //console.log($event.target.value)
